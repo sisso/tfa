d0cf3a189017471311a7f03cdc35b0d9375b5c06
sisso <sissobr@gmail.com>
2022-06-08 21:07:41 -0300

    get and post keys

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use rouille::Request;
 use rouille::Response;
 use serde::Serialize;
+use std::cell::RefCell;
+use std::io::Read;
+use std::sync::Mutex;
 
 #[derive(Serialize)]
 struct KeyResp {
-    key: Option<String>,
+    value: Option<Value>,
+}
+
+type Key = String;
+type Value = String;
+
+struct State {
+    requests: Vec<(Key, Option<Value>)>,
 }
 
 fn main() {
@@ -12,13 +22,49 @@ fn main() {
         .filter(None, log::LevelFilter::Debug)
         .init();
 
+    let state = Mutex::new(State { requests: vec![] });
+
     rouille::start_server("0.0.0.0:8881", move |request| {
-        if request.url().starts_with("/keys/") {
+        let url = request.url();
+
+        if url.starts_with("/keys/") {
             log::debug!("{:?}", request);
 
+            let key = &url["/keys/".len()..];
+            log::info!("key {}", key);
+
+            let mut state = state.lock().unwrap();
+            let index = state.requests.iter().position(|(k, v)| k == key);
+            let data = request.data();
+
             match request.method() {
-                "GET" => return Response::json(&KeyResp { key: None }),
-                "POST" => return Response::empty_204(),
+                "GET" if index.is_none() => {
+                    state.requests.push((key.to_string(), None));
+                    return Response::json(&KeyResp { value: None });
+                }
+
+                "GET" => {
+                    return Response::json(&KeyResp {
+                        value: state.requests[index.unwrap()].1.take(),
+                    });
+                }
+                "POST" if index.is_none() => {
+                    return Response::empty_404();
+                }
+
+                "POST" if data.is_none() => {
+                    return Response::empty_400();
+                }
+
+                "POST" => {
+                    let mut rb = data.unwrap();
+                    let mut value = String::new();
+                    rb.read_to_string(&mut value)
+                        .expect("fail to parse request body as string");
+
+                    state.requests[index.unwrap()].1 = Some(value);
+                    return Response::empty_204();
+                }
                 _ => {}
             }
         }
